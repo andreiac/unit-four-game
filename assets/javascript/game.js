87cec0ed8b1f46ddbe80314f59e83bfc5a89d39e
Your Name <andreia@andreias-mbp.attlocal.net>
2019-07-06 09:53:03 -0400

    finalized html and css, added pseudocoding to js

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+//create array of players
+
+//create variables for each character's attack power
+
+//create variables for each character's HP
+
+//when i select my player, put all other characters into enemies row and change html from "pick your character" to "Your character"
+
+//when i click on an enemy, move that character into defender row
+
+//when  i click attack button, myplayer attacks enemy
+
+//everytime i press attack, myPlayer gains attack power, taking more HP from enemies
+
+//screen tells me what damaged I caused to enemy and what damage enemy caused me
+
+//if myPlayer triumphs over enemy, then I pick new enemy to play. loop this
+
+//if myplayer HP gets to zero or below-- game over
+//lost? then disable attack button and show button to restart game
+
+//if no more enemies to fight, then i win. 
+//if win, show win message and display button to restart game.
